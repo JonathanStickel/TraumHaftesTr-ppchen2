8671a2051daae9133bca09743ae65492cc7c35ca
SchaulLP <SCH210425@spengergasse.at>
2023-02-02 20:38:34 +0100

    finished tutorial

--- TraumhaftesTrüppchen/Assets/Paul/Scripts/TextChange.cs
+++ TraumhaftesTrüppchen/Assets/Paul/Scripts/TextChange.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class TextChange : MonoBehaviour
 {
     public Text text;
+    public bool isNearSpikes;
     // Start is called before the first frame update
     void Start()
     {
+        isNearSpikes = false;
         StartCoroutine(tutorial());
     }
 
@@ -20,7 +22,15 @@ public class TextChange : MonoBehaviour
     IEnumerator tutorial() 
     {
         yield return new WaitForSeconds(2);
-        text.text = "Get close to an Object and press Leftclick to drag it";
+        text.text = "Get close to an object and press leftclick to drag it";
+        // if (isNearSpikes == true)
+        yield return new WaitForSeconds(8f);
+            text.text = "Thorns can kill you if you step on them";
+            yield return new WaitForSeconds(2);
+            text.text = "If you die your corpse will remain ";
+            yield return new WaitForSeconds(2);
+            text.text = "You can use corpses as solid objects ";
+        
 
     }
 }
